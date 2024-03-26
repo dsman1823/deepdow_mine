66fd84621f69e0d6a9f40794b38f7b619ff82816
dsman1823 <37937854+dsman1823@users.noreply.github.com>
2024-03-26 14:11:47 +0100

    Update allocate.py

--- deepdowmine/layers/allocate.py
+++ deepdowmine/layers/allocate.py
@@ -9,6 +9,37 @@ import torch.nn as nn
 from .misc import Cov2Corr, CovarianceMatrix, KMeans
 
 
+
+class ThesisMarkowitzFullOpti(nn.Module):
+    def __init__(self, n_assets, max_weight=1):
+        super(ThesisMarkowitz, self).__init__()
+        self.n_assets = n_assets
+        self.max_weight = max_weight
+        # Static CVXPY parameters initialized without values
+        self.rets_param = cp.Parameter(n_assets)
+        self.alpha_param = cp.Parameter(nonneg=True)
+        self.gamma_param = cp.Parameter(nonneg=True)
+        self.covmat_param = cp.Parameter((n_assets, n_assets), PSD=True)
+
+        w = cp.Variable(n_assets)
+        ret = cp.matmul(self.rets_param, w)
+        risk = self.gamma_param * cp.sqrt(cp.quad_form(w, self.covmat_param))
+        reg = self.alpha_param * cp.norm(w, 2)
+
+        prob = cp.Problem(cp.Maximize(ret - risk - reg),
+                          [cp.sum(w) == 1, w <= self.max_weight, -w <= self.max_weight])
+        self.cvxpylayer = CvxpyLayer(prob, parameters=[self.rets_param, self.covmat_param, self.alpha_param, self.gamma_param], variables=[w])
+
+    def forward(self, rets, covmat, gamma, alpha):
+        # Ensure alpha and gamma are non-negative
+        alpha_abs = torch.abs(alpha)
+        gamma_abs = torch.abs(gamma)
+
+        optimal_weights, = self.cvxpylayer(rets, covmat, alpha_abs, gamma_abs)
+        return optimal_weights
+
+
+
 class ThesisMarkowitz(nn.Module):
     def __init__(self, n_assets, max_weight=1):
         super(ThesisMarkowitz, self).__init__()
